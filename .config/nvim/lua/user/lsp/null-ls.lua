82fddc3f93794050900cd4d17adfabfdbdde843b
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2023-01-21 16:36:18 +0700

    Add markdownlint. Toggleterm horizontal instead of float

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -1,28 +1,32 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-	return
+    return
 end
 
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier.with({
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"css",
-				"scss",
-				"json",
-				"yaml",
-				"markdown",
-				"html",
-			},
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		}),
-		null_ls.builtins.diagnostics.flake8.with({
-			filetypes = { "python" },
-		}),
-	},
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = {
+                "javascript",
+                "javascriptreact",
+                "typescript",
+                "typescriptreact",
+                "css",
+                "scss",
+                "json",
+                "yaml",
+                "html",
+            },
+            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+        }),
+        null_ls.builtins.formatting.markdownlint.with({
+            filetypes = {
+                "markdown",
+            },
+        }),
+        null_ls.builtins.diagnostics.flake8.with({
+            filetypes = { "python" },
+        }),
+    },
 })
