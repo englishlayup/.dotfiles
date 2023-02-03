a286e63e16b56a5a7770acbb4df38f1350b27eb8
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2023-02-03 09:26:29 -0500

    Format options.lua

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,29 +1,29 @@
 local o = vim.opt
-o.tabstop=4
-o.softtabstop=4
-o.expandtab=true
-o.shiftwidth=4
-o.autoindent=true
-o.relativenumber=true
-o.number=true
-o.wildmode={"list", "longest"}
-o.signcolumn="auto"
-o.scrolloff=8
-o.cursorline=true
-o.ignorecase=true
-o.smartcase=true
-o.showmatch=true
-o.hlsearch=false
-o.incsearch=true
-o.showcmd=true
-o.updatetime=50
-o.errorbells=false
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
+o.shiftwidth = 4
+o.autoindent = true
+o.relativenumber = true
+o.number = true
+o.wildmode = { "list", "longest" }
+o.signcolumn = "auto"
+o.scrolloff = 8
+o.cursorline = true
+o.ignorecase = true
+o.smartcase = true
+o.showmatch = true
+o.hlsearch = false
+o.incsearch = true
+o.showcmd = true
+o.updatetime = 50
+o.errorbells = false
 o.swapfile = false
 o.backup = false
 o.undodir = os.getenv('HOME') .. '/.vim/undodir'
 o.undofile = true
-o.termguicolors=true
-o.mouse=""
+o.termguicolors = true
+o.mouse = ""
 -- FINDING FILES:
 
 -- Search down into subfolders
@@ -31,14 +31,14 @@ o.mouse=""
 o.path:append('**')
 
 -- Display all matching files when we tab complete
-o.wildmenu=true
-o.wildignore={
+o.wildmenu = true
+o.wildignore = {
     '**/node_modules/**',
-    '.hg,.git,.svn',                       -- Version control 
-    '*.jpg,*.bmp,*.gif,*.png,*.jpeg',      -- binary images
-    '*.o,*.obj,*.exe,*.dll,*.manifest',    -- compiled object files
-    '*.pyc',                               -- Python byte code
-    }
+    '.hg,.git,.svn', -- Version control
+    '*.jpg,*.bmp,*.gif,*.png,*.jpeg', -- binary images
+    '*.o,*.obj,*.exe,*.dll,*.manifest', -- compiled object files
+    '*.pyc', -- Python byte code
+}
 -- NOW WE CAN:
 -- - Hit tab to :find by partial match
 -- - Use * to make it fuzzy
