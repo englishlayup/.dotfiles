5627944cbfe953f02c0f10345212d25ce014853f
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2023-02-22 12:04:27 -0500

    Fix error when running PackerInstall for the first time
    Also format some lua files

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 vim.cmd([[
@@ -18,74 +18,78 @@ vim.cmd([[
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'nvim-lua/plenary.nvim'
-    -- Colorschemes
-    use 'ellisonleao/gruvbox.nvim'
-    use 'folke/tokyonight.nvim'
-    -- Enhances netrw
-    use 'tpope/vim-vinegar'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	-- Colorschemes
+	use("ellisonleao/gruvbox.nvim")
+	use("folke/tokyonight.nvim")
+	-- Enhances netrw
+	use("tpope/vim-vinegar")
+	use("tpope/vim-repeat")
+	use("tpope/vim-surround")
+	-- LSP
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	-- Format and Linters
+	use("jose-elias-alvarez/null-ls.nvim")
+	-- Auto completion
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	-- Snippet engine
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	-- Syntax highlighting
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+	use("nvim-treesitter/nvim-treesitter-textobjects")
+	-- Brackets stuff
+	use("p00f/nvim-ts-rainbow")
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup({
+				indent_blankline_show_first_indent_level = false,
+				show_current_context = true,
+				show_current_context_start = true,
+			})
+		end,
+	})
+	-- Github Copilot
+	use("github/copilot.vim")
+	-- Toggle terminal
+	use("akinsho/toggleterm.nvim")
 
-    use 'tpope/vim-repeat'
-    use 'tpope/vim-surround'
-    -- LSP
-    use 'williamboman/mason.nvim'
-    use "williamboman/mason-lspconfig.nvim"
-    use 'neovim/nvim-lspconfig'
-    -- Format and Linters
-    use 'jose-elias-alvarez/null-ls.nvim'
-    -- Auto completion
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    -- Snippet engine
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
-    -- Syntax highlighting
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-    }
-    use 'nvim-treesitter/nvim-treesitter-textobjects'
-    -- Brackets stuff
-    use 'p00f/nvim-ts-rainbow'
-    use {
-        'windwp/nvim-autopairs',
-        config = function() require('nvim-autopairs').setup {} end
-    }
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
-    use {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-        end
-    }
-    use {
-        'lukas-reineke/indent-blankline.nvim',
-        config = function()
-            require('indent_blankline').setup {
-                indent_blankline_show_first_indent_level = false,
-                show_current_context = true,
-                show_current_context_start = true,
-            }
-        end
-    }
-    -- Github Copilot
-    use 'github/copilot.vim'
-    -- Toggle terminal
-    use 'akinsho/toggleterm.nvim'
-
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
