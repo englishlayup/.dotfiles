d6cbe366ab0f222eb7ef3b2932704b8e99ce3a68
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2023-10-14 11:35:06 -0400

    Move diagnostic keymaps to keymaps.lua

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -1,16 +1,3 @@
-local b_status_ok, telescope_builtin = pcall(require, 'telescope.builtin')
-if not b_status_ok then
-    return
-end
-
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', telescope_builtin.diagnostics, opts)
-
 local signs = {
     Error = '',
     Warn = '',
