f0530dd3e2dc023c8e06906c7bb7d8e706fbac4c
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-02-24 13:21:03 -0500

    color: use gruvbox theme for lualine

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -113,11 +113,12 @@ require('lazy').setup {
     {
         -- Set lualine as statusline
         'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
         -- See `:help lualine.txt`
         opts = {
             options = {
                 icons_enabled = false,
-                theme = 'solarized_dark',
+                theme = 'gruvbox',
                 component_separators = '|',
                 section_separators = '',
             },
