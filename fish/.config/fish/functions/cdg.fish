c57d2408972d80cd2a5398fb43dd3ea8a48f84e1
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-09-16 08:46:47 -0400

    fix: cdg without args

--- fish/.config/fish/functions/cdg.fish
+++ fish/.config/fish/functions/cdg.fish
@@ -1,3 +1,7 @@
 function cdg -d "cd relative to git root"
-    cd (git rev-parse --show-toplevel 2>/dev/null)/$argv[1]
+    if test (count $argv) -eq 1
+        cd (git rev-parse --show-toplevel 2>/dev/null)/$argv[1]
+    else if test (count $argv) -eq 0
+        cd (git rev-parse --show-toplevel 2>/dev/null)
+    end
 end
