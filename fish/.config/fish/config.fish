0c89c38927314c6d3ff7f1477319b61646763edc
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2024-09-24 09:16:56 -0400

    feat: abbr for tmux. set EDITOR to nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_hybrid_key_bindings
+    abbr -a ta 'tmux attach -t'
+    abbr -a tad 'tmux attach -d -t'
+    abbr -a ts 'tmux new-session -s'
+    abbr -a tl 'tmux list-sessions'
+    abbr -a tksv 'tmux kill-server'
+    abbr -a tkss 'tmux kill-session -t'
+    set -gx EDITOR nvim
 end
