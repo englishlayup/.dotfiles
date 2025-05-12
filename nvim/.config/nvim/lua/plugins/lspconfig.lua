8c63635845a7c95c85ab90f795fde2a32ac92f7c
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2025-05-12 18:06:27 -0400

    use v11 lsp config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -33,7 +33,8 @@ return {
     config = function(_, opts)
       for server, config in pairs(opts.servers) do
         config.capabilities = require 'blink.cmp'.get_lsp_capabilities(config.capabilities)
-        require 'lspconfig'[server].setup(config)
+        vim.lsp.config(server, config)
+        vim.lsp.enable { server }
       end
     end,
   }
